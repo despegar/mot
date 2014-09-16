19a432be44330cbe94d3f30958f7918bd50c14db
Mariano Barrios <marbar@gmail.com>
2014-09-16 11:24:19 -0300

    Small optimization

--- src/main/scala/mot/exceptions.scala
+++ src/main/scala/mot/exceptions.scala
@@ -1,8 +1,22 @@
 package mot
 
+// Common
+
+/* Useful to speed up exceptions that are not thrown at creation point */
+trait NoStackTraceException extends Exception {
+  override def fillInStackTrace() = this
+}
+
+class BadDataException(msg: String) extends Exception(msg)
+
+class UncompatibleProtocolVersion(message: String) extends Exception(message)
+
+class MessageTooLargeException(size: Int, maxSize: Int) 
+  extends Exception(s"Message is bigger than the maximum allowed by the other party ($size > $maxSize)")
+
 // Client
 
-class ResponseTimeoutException extends Exception("Response timed out.")
+class ResponseTimeoutException extends Exception("Response timed out.") with NoStackTraceException
 
 class ClientClosedException extends Exception
 
@@ -23,11 +37,3 @@ class ResponseAlreadySendException extends Exception("Message has already been r
 class InvalidServerConnectionException(cause: Throwable)
   extends Exception("Cannot send response because server the connection from which the request came was terminated", cause)
 
-// Common
-
-class BadDataException(msg: String) extends Exception(msg)
-
-class UncompatibleProtocolVersion(message: String) extends Exception(message)
-
-class MessageTooLargeException(size: Int, maxSize: Int) 
-  extends Exception(s"Message is bigger than the maximum allowed by the other party ($size > $maxSize)")
