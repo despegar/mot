599ba5d48e1c2236e8cbf99fd5c6e857f7b2e271
Mariano Barrios <marbar@gmail.com>
2014-09-23 01:04:55 -0300

    Change message type values

--- src/main/scala/mot/message/MessageType.scala
+++ src/main/scala/mot/message/MessageType.scala
@@ -2,7 +2,7 @@ package mot.message
 
 object MessageType extends Enumeration {
   val Hello = Value(0)
-  val Heartbeat = Value(2)
-  val Response = Value(4)
-  val Message = Value(5)
+  val Heartbeat = Value(1)
+  val Message = Value(2)
+  val Response = Value(3)
 }
