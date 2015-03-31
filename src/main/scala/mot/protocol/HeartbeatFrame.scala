661cdf46a592e150b0904145c06ce181f8007254
Mariano Barrios <marbar@gmail.com>
2015-03-31 13:44:42 -0300

    Add comment

--- src/main/scala/mot/protocol/HeartbeatFrame.scala
+++ src/main/scala/mot/protocol/HeartbeatFrame.scala
@@ -4,7 +4,8 @@ import mot.buffer.ReadBuffer
 import mot.buffer.WriteBuffer
 
 /**
- * Empty frame that keeps the connection with some activity when there are not other kind of frames.
+ * Empty frame that keeps the connection with some activity when there are not other kind of frames. The protocol 
+ * mandates to send a heartbeat after 5 seconds of not sending anything.
  */
 case class HeartbeatFrame() extends Frame {
   
